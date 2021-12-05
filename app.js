3362b2486ce57f7fd01ab3885ace09fa0942ae97
Alex Soucy <asoucy22@yahoo.com>
2021-12-05 18:42:31 -0500

    added functionality to generate HTML page

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
 // console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// const printProfileData = profileDataArr => {
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error (err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
