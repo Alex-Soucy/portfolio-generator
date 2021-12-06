bc2215a2df23f4e9a6faa817667dd30038898216
Alex Soucy <asoucy22@yahoo.com>
2021-12-05 19:11:48 -0500

    added inquirer functionality

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
 
-const [name, github] = profileDataArgs;
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+// const profileDataArgs = process.argv.slice(2);
+
+// const [name, github] = profileDataArgs;
 // console.log(profileDataArgs);
 
 // const printProfileData = profileDataArr => {
@@ -14,9 +28,9 @@ const [name, github] = profileDataArgs;
 // printProfileData(profileDataArgs);
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error (err);
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
